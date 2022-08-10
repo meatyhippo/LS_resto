d457ba4600b8523d8df6d2fd9737cad49fa20677
rubenvanhee <37211963+rubenvanhee@users.noreply.github.com>
2022-08-10 10:06:36 +0200

    commit on main

--- LSK/help_tool.js
+++ LSK/help_tool.js
@@ -5,35 +5,100 @@
 		htmlnode.id = "overlay-box"
 		stylenode.innerHTML = `
 		#overlay-box{
-				width: 100%;
-				height: 500px;
-				top: 0;
-				left: 0;
-				background-color: rgba(0,0,0,.6)!important;
-				z-index: 9999999;
-				display: block;
-				position: fixed;
-				/* opacity: 0.7; */
+			height: 100%;
+			width: 100%;
+			top: 0;
+			left: 0;
+			z-index: 9999999;
+			display: block;
+			position: fixed;
+			float: left;
+			/* opacity: 0.7; */
+		}
+		#close-box{
+			height: 100%;
+			width: 70%;
+			top: 0;
+			left: 0;
+			z-index: 99999999;
+			float: left;
 		}
 		#ui-box{
+			background-color: rgba(0,0,0,.8)!important;
 			color: white;
 			height: 100%;
-			width: 450px;
+			width: 30%;
+			top: 0;
+			right: 0;
 			float: right;
-			z-index:99999999999999;
-		}`;
+			z-index: 99999999;
+			padding: ;
+		}
+		#ui-box label, #ui-box input {
+			width: 50%;
+		}
+		`;
 		htmlnode.innerHTML = `
+		<div id="close-box">
+		</div>
 		<div id="ui-box">
-			<div class="account-extend">
-				<p>TEXT here</p>
-				<table class="">
-				</table>
+			<div class="" style="">
+				<div id="extend_account">
+					<p>
+						<b>Extend account</b>
+						<br>
+						fill in specific date or leave empty for extend until 2030
+					</p>
+				</div>
+				<div id=""></div>
 			</div>
 		</div>`;
 		document.body.append(htmlnode, stylenode);
 	}
+	function addfunc(title){
+		switch (title) {
+			case 'extend_account':
+				//get extend account function
+				$.get("https://meatyhippo.github.io/LS_resto/LSK/scripts/extend_account.js",
+					function (data, textStatus, jqXHR) {
+						/**/console.log(title, 'function loaded: ', data);
+					},
+				);
+				//set input fields
+				extend = document.createElement('div'),
+				blID = document.createElement('input'),
+				date = document.createElement('input'),
+				month = document.createElement('input'),
+				year = document.createElement('input'),
+				button = document.createElement('button'),
+				blIDLabel = document.createElement('label'),
+				dateLabel = document.createElement('label'),
+				monthLabel = document.createElement('label'),
+				yearLabel = document.createElement('label');
+				location.pathname.includes('/businessLocationDetails')? 
+				$(blID).attr({'placeholder': location.search.match(/\d+/gm)[0],'id':'blID'}):
+				$(blID).attr({'placeholder': '123456789','id':'blID'});
+				$(date).attr({'placeholder': '1','id':'date'}),
+				$(month).attr({'placeholder': '1','id':'month'}),
+				$(year).attr({'placeholder': '2030','id':'year'}),
+				$(blIDLabel).attr('for','blID'),
+				$(dateLabel).attr('for','date'),
+				$(monthLabel).attr('for','month'),
+				$(yearLabel).attr('for','year');
+				//add fields to div
+				extend.append(blID,blIDLabel,date,dateLabel,month,monthLabel,year,yearLabel,button);
+				//paint div to ui box
+				document.getElementById(title).append(extend);
+				//add properties to fields
+
+				break;
+		
+			default:
+				break;
+		}
+	}
 	let createbox = new Promise((resolve, reject)=>{
-		resolve(paint_box())
+		resolve(paint_box());
 	});
 	createbox.then((fullfilled)=>{
 		/**/console.log($('#ui-box'));
@@ -41,11 +106,14 @@
 			e.preventDefault();
 			return;
 		});
-		$(htmlnode).click((e)=>{
+		$('#close-box').click((e)=>{
 			htmlnode.remove();
 			stylenode.remove();
 			/**/console.log(htmlnode)
-		})
+		});
+		if (location.pathname.includes('/admin')){
+			addfunc('extend_account');
+		}
 	})
 	.catch((e)=>{/**/console.log(e);})
 })();
--- LSK/scripts/extend_account.js
+++ LSK/scripts/extend_account.js
@@ -1,20 +1,22 @@
-fetch("https://manager.trial.lsk.lightspeed.app/admin/setExpirationDate", {
-  "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-    "cache-control": "max-age=0",
-    "content-type": "application/x-www-form-urlencoded",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "sec-gpc": "1",
-    "upgrade-insecure-requests": "1"
-  },
-  "referrer": "https://manager.trial.lsk.lightspeed.app/admin/businessLocationDetails?blId=183455233081346",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "blId=183455233081346&expirationDate=date.struct&expirationDate_day=20&expirationDate_month=2&expirationDate_year=2030",
-  "method": "POST",
-  "mode": "cors",
-  "credentials": "include"
-});
+function extend_account(BLid, day, month, year){
+	fetch(location.origin+"/admin/setExpirationDate", {
+	"headers": {
+		"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+		"cache-control": "max-age=0",
+		"content-type": "application/x-www-form-urlencoded",
+		"sec-fetch-dest": "document",
+		"sec-fetch-mode": "navigate",
+		"sec-fetch-site": "same-origin",
+		"sec-fetch-user": "?1",
+		"sec-gpc": "1",
+		"upgrade-insecure-requests": "1"
+	},
+	"body": `blId=${BLid}&expirationDate=date.struct&expirationDate_day=${day||28}&expirationDate_month=${month||5}&expirationDate_year=${year}`,
+	"method": "POST",
+	"mode": "cors",
+	"credentials": "include"
+	})
+	.then(res => /**/console.log('extended:', res))
+	.catch(e=> /**/console.log('error extending:', e))
+}
